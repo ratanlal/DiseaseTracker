2c9c1996914fd4918c224f457b573f2dc72623bb
ratanlal <ratanlal5793@gmail.com>
2019-04-19 15:57:12 +0530

    add first screen of the game and initial commit

---
+++ app/src/main/java/com/diseasetracker/diseasetracker/MainActivity.java
@@ -0,0 +1,23 @@
+package com.diseasetracker.diseasetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+   /* TextView diseaseTrackerText;
+    ImageView diseaseTrackerLogo;
+    Button startGameButton;*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*diseaseTrackerText.findViewById(R.id.disease_tracker_text);
+        diseaseTrackerLogo.findViewById(R.id.disease_tracker_logo);
+        startGameButton.findViewById(R.id.start_game_button);*/
+    }
+}
